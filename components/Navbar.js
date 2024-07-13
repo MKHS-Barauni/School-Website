045a11b42754ead36cd2d67cb5404da833144074
monkepi18 <patelpiyush1810@gmail.com>
2024-07-14 01:28:52 +0530

    all disclosures added

--- components/Navbar.js
+++ components/Navbar.js
@@ -121,6 +121,79 @@ const Navbar = () => {
             </ul>
           </div>
         </span>
+        <span className={`text-orange-400 hover:border-orange-400 dropdown dropdown-hover dropdown-end cursor-pointer w-32 px-4 text-center py-2 hover:font-bold border-b-2 border-black hover:border-b-2`}>
+          <label tabIndex={0} className='hover:text-orange-400 m-1 relative text-white'>
+          <Link href='/'>
+          <span className={"text-white"}>
+            Disclosures
+          </span>
+        </Link>
+          </label>
+          <div className='absolute top-[42px] -left-10 dropdown-content p-4'>
+            <ul tabIndex={0} className="text-white text-[17px] font-semibold menu p-2 shadow bg-stone-800 rounded-xl w-52 flex flex-col items-start">
+              <Link href='/TrustCertificate'>
+                <li className={`${router.pathname == "/more#activities" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                  Trust Certificate
+                </li>
+              </Link>
+              <Link href='/NOC'>
+                <li className={`${router.pathname == "/more#achievements" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                  NOC
+                </li>
+              </Link>
+              <Link href='/RecognitionCertificate' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                Recognition Certificate
+                </li>
+              </Link>
+              <Link href='/BuildingCertificate' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                Building Certificate
+                </li>
+              </Link>
+              <Link href='/FireCertificate' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                Fire Certificate
+                </li>
+              </Link>
+              <Link href='/DEOCertificate' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                DEO Certificate
+                </li>
+              </Link>
+              <Link href='/WaterHealthCertificate' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                Water Health Certificate
+                </li>
+              </Link>
+              <Link href='/FeeStructure' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                Fee Structure
+                </li>
+              </Link>
+              <Link href='/AcademicCalender' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                Academic Calendar
+                </li>
+              </Link>
+              <Link href='/SMC' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                List of SMC
+                </li>
+              </Link>
+              <Link href='/PTA' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                List of PTA
+                </li>
+              </Link>
+              <Link href='/ThreeYearResult' scroll={false}>
+                <li className={`${router.pathname == "/more#media" ? "hover-border-orange-400 text-orange-400" : ""} hover-bg-orange-300 hover-text-black rounded-lg w-full text-left p-2`}>
+                3 Year Results
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </span>
         <Link href='/Committee'>
           <span className={`${router.pathname == "/Committee" ? "text-orange-400" : "text-white"} cursor-pointer w-32 px-4 text-center py-2 hover:font-bold border-b-2 border-black hover:border-b-2 hover:text-orange-400 hover:border-orange-400`}>
             Our Team
---
+++ pages/WaterHealthCertificate.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const WaterHealthCertificate = () => {
+  return (
+    <main className="flex-grow overflow-x-clip bg-gray-50 pt-14">
+      <div className="min-h-full bg-gradient-to-r from-white to-indigo-50">
+        <div className="relative flex-grow items-start justify-start space-x-2 px-4 md:flex md:flex-row md:px-8 xl:px-10">
+          <main className="w-full pt-4 md:flex-grow">
+            <div className="flex flex-col items-center justify-center px-4 py-2 sm:p-6">
+              <h1 className="text-center text-black text-4xl sm:text-6xl font-bold leading-[3rem] border-b-4 border-blue-500 pb-1 md:pb-3">Water Health Certificate</h1>
+            </div>
+            <div className="flex justify-center mt-4">
+              <iframe 
+                src="/Water and Hygienic.pdf" 
+                width="80%" 
+                height="600px" 
+                title="Trust Certificate" 
+                className="border-2 border-blue-500"
+              >
+              </iframe>
+            </div>
+          </main>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default WaterHealthCertificate
