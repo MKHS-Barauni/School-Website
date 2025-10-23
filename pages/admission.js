fbf08ab84c893961a6153534c97166c4b21cb156
Arpit Mishra <162034001+23f3003894@users.noreply.github.com>
2025-10-23 22:27:41 +0530

    Trying a new commit by deleting one comments line from admission.js

--- pages/admission.js
+++ pages/admission.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import Lottie from "lottie-react";
 import dynamic from "next/dynamic";
 const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 import examsPreparation from "../lottieFiles/examsPreparation.json"
